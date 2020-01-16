3331cd84e48f52f6a732eb56dc4d67f36fda0edc
Charles Campista <charlescampista@gmail.com>
2020-01-16 14:02:09 -0200

    Created the Quick Start Project

---
+++ MyC#Samples/CharlesCSHARPSamples/QuickStarter/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace QuickStarter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.path = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(567, 208);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open File";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Location = new System.Drawing.Point(53, 208);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowser.TabIndex = 1;
+            this.btnBrowser.Text = "Search";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // path
+            // 
+            this.path.AutoSize = true;
+            this.path.Location = new System.Drawing.Point(50, 34);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(0, 13);
+            this.path.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 275);
+            this.Controls.Add(this.path);
+            this.Controls.Add(this.btnBrowser);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.Label path;
+    }
+}
+
---
+++ MyC#Samples/CharlesCSHARPSamples/QuickStarter/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuickStarter
+{
+    public partial class Form1 : Form
+    {
+        private string _path = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string BrowseFile()
+        {
+            
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "*.exe|*.EXE";
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                return openFileDialog.FileName;
+            }
+            return "";
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            this._path = BrowseFile();
+            path.Text = this._path;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(this._path))
+                Process.Start(this._path);
+        }
+    }
+}
